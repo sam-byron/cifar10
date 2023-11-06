70f2e2971e485836d18cecd12980e151bfafd40a
Sam Byron <mrc4090@proton.me>
2023-11-06 14:38:44 -0800

    Enlarge model. Visualize reconstructed samples

--- pretraining.py
+++ pretraining.py
@@ -5,12 +5,15 @@ import tensorflow as tf
 # LOAD DATASET
 (x_train, _), (x_test, _) = tf.keras.datasets.cifar10.load_data()
 
+# x_train = x_train[1:10000]
+
 # HYPER-PARAMETERS
-EPOCHS = 20
+EPOCHS = 10
 # set the dimensionality of the latent space to a plane for visualization later
 LATENT_DIM = 100
 LEARNING_RATE = 0.001
 BATCH_SIZE = 128
+DROPOUT = 0.2
 DATA_SHAPE = x_train.shape[1:]
 train_size = x_train.shape[0]
 test_size = x_test.shape[0]
@@ -18,18 +21,16 @@ test_size = x_test.shape[0]
 # ENCODER
 
 encoder_inputs = keras.Input(shape=DATA_SHAPE)
-x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(encoder_inputs)
-x = layers.Conv2D(128, 3, activation="relu", strides=2, padding="same")(x)
-x = layers.Conv2D(256, 3, activation="relu", strides=1, padding="same")(x)
+x = layers.Conv2D(128, 3, activation="relu", strides=2, padding="same")(encoder_inputs)
+x = layers.Conv2D(256, 3, activation="relu", strides=2, padding="same")(x)
+x = layers.SpatialDropout2D(DROPOUT)(x)
+x = layers.Conv2D(512, 3, activation="relu", strides=1, padding="same")(x)
 x = layers.Flatten()(x)
-x = layers.Dense(16, activation="relu")(x)
 z_mean = layers.Dense(LATENT_DIM, name="z_mean")(x)
 z_log_var = layers.Dense(LATENT_DIM, name="z_log_var")(x)
 encoder = keras.Model(encoder_inputs, [z_mean, z_log_var], name="encoder")
 
 encoder.summary()
-
-# DECODER
 
 class Sampler(layers.Layer):
     def call(self, z_mean, z_log_var):
@@ -38,12 +39,14 @@ class Sampler(layers.Layer):
         epsilon = tf.random.normal(shape=(batch_size, z_size))
         return z_mean + tf.exp(0.5 * z_log_var) * epsilon
     
+# DECODER
+    
 latent_inputs = keras.Input(shape=(LATENT_DIM,))
-x = layers.Dense(8 * 8 * 256, activation="relu")(latent_inputs)
-x = layers.Reshape((8, 8, 256))(x)
-x = layers.Conv2DTranspose(256, 3, activation="relu", strides=1, padding="same")(x)
+x = layers.Dense(8 * 8 * 512, activation="relu")(latent_inputs)
+x = layers.Reshape((8, 8, 512))(x)
+x = layers.Conv2DTranspose(512, 3, activation="relu", strides=1, padding="same")(x)
+x = layers.Conv2DTranspose(256, 3, activation="relu", strides=2, padding="same")(x)
 x = layers.Conv2DTranspose(128, 3, activation="relu", strides=2, padding="same")(x)
-x = layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(x)
 decoder_outputs = layers.Conv2D(3, 3, activation="sigmoid", padding="same")(x)
 decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
 
@@ -103,3 +106,31 @@ x_test = x_test / 255
 vae = VAE(encoder, decoder)
 vae.compile(optimizer=keras.optimizers.Adam(), run_eagerly=True)
 vae.fit(x_train, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=2, callbacks=[callbacks.ProgbarLogger(count_mode="steps")])
+
+z_mean, z_log_var = vae.encoder.predict(x_test)
+sampler = Sampler()
+z = sampler(z_mean, z_log_var)
+decoded_imgs = decoder.predict(z)
+
+import matplotlib.pyplot as plt
+
+n = 10
+plt.figure(figsize=(20, 4))
+for i in range(n):
+  # display original
+  ax = plt.subplot(2, n, i + 1)
+  plt.imshow(x_test[i])
+  plt.title("original")
+  plt.gray()
+  ax.get_xaxis().set_visible(False)
+  ax.get_yaxis().set_visible(False)
+
+  # display reconstruction
+  ax = plt.subplot(2, n, i + 1 + n)
+  plt.imshow(decoded_imgs[i])
+  plt.title("reconstructed")
+  plt.gray()
+  ax.get_xaxis().set_visible(False)
+  ax.get_yaxis().set_visible(False)
+plt.show()
+
