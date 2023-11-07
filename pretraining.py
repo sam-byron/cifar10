324f74abcbbc9ce3f23d0ba0f3a634997f60493d
Sam Byron <mrc4090@proton.me>
2023-11-07 10:44:41 -0800

    Support save and loading weights from file

--- pretraining.py
+++ pretraining.py
@@ -3,14 +3,23 @@ from tensorflow.keras import layers, callbacks
 import tensorflow as tf
 
 # LOAD DATASET
-(x_train, _), (x_test, _) = tf.keras.datasets.cifar10.load_data()
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+x_train = x_train / 255
+x_test = x_test / 255
 
 # x_train = x_train[1:10000]
 
+# CONTROL PARAMETERS
+PRETRAIN = False
+SAVE_VAE = False
+USE_PRETRAIN = True
+VAE_LOCATION = '/home/sambyron/engineering/ML/tensorflow/cifar10/models/vae_weights'
+
+
 # HYPER-PARAMETERS
-EPOCHS = 10
+EPOCHS = 5
 # set the dimensionality of the latent space to a plane for visualization later
-LATENT_DIM = 100
+LATENT_DIM = 1024
 LEARNING_RATE = 0.001
 BATCH_SIZE = 128
 DROPOUT = 0.2
@@ -23,7 +32,7 @@ test_size = x_test.shape[0]
 encoder_inputs = keras.Input(shape=DATA_SHAPE)
 x = layers.Conv2D(128, 3, activation="relu", strides=2, padding="same")(encoder_inputs)
 x = layers.Conv2D(256, 3, activation="relu", strides=2, padding="same")(x)
-x = layers.SpatialDropout2D(DROPOUT)(x)
+# x = layers.SpatialDropout2D(DROPOUT)(x)
 x = layers.Conv2D(512, 3, activation="relu", strides=1, padding="same")(x)
 x = layers.Flatten()(x)
 z_mean = layers.Dense(LATENT_DIM, name="z_mean")(x)
@@ -100,37 +109,85 @@ class VAE(keras.Model):
 
 import numpy as np
 
-x_train = x_train / 255
-x_test = x_test / 255
-
 vae = VAE(encoder, decoder)
 vae.compile(optimizer=keras.optimizers.Adam(), run_eagerly=True)
-vae.fit(x_train, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=2, callbacks=[callbacks.ProgbarLogger(count_mode="steps")])
+if PRETRAIN:
+    vae.fit(x_train, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=2, callbacks=[callbacks.ProgbarLogger(count_mode="steps")])
 
-z_mean, z_log_var = vae.encoder.predict(x_test)
-sampler = Sampler()
-z = sampler(z_mean, z_log_var)
-decoded_imgs = decoder.predict(z)
+    if SAVE_VAE:
+        vae.save_weights(VAE_LOCATION)
+        # vae.save(VAE_LOCATION, save_format="tf")
 
-import matplotlib.pyplot as plt
+    z_mean, z_log_var = vae.encoder.predict(x_test)
+    sampler = Sampler()
+    z = sampler(z_mean, z_log_var)
+    decoded_imgs = decoder.predict(z)
 
-n = 10
-plt.figure(figsize=(20, 4))
-for i in range(n):
-  # display original
-  ax = plt.subplot(2, n, i + 1)
-  plt.imshow(x_test[i])
-  plt.title("original")
-  plt.gray()
-  ax.get_xaxis().set_visible(False)
-  ax.get_yaxis().set_visible(False)
+    import matplotlib.pyplot as plt
 
-  # display reconstruction
-  ax = plt.subplot(2, n, i + 1 + n)
-  plt.imshow(decoded_imgs[i])
-  plt.title("reconstructed")
-  plt.gray()
-  ax.get_xaxis().set_visible(False)
-  ax.get_yaxis().set_visible(False)
-plt.show()
+    n = 10
+    plt.figure(figsize=(20, 4))
+    for i in range(n):
+    # display original
+        ax = plt.subplot(2, n, i + 1)
+        plt.imshow(x_test[i])
+        plt.title("original")
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
 
+        # display reconstruction
+        ax = plt.subplot(2, n, i + 1 + n)
+        plt.imshow(decoded_imgs[i])
+        plt.title("reconstructed")
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    plt.show()
+
+
+# CLASSIFICATION
+from tensorflow.keras import initializers, optimizers
+import os
+
+initializer = initializers.GlorotNormal()
+
+cnn_inputs = keras.Input(shape=DATA_SHAPE)
+x = layers.Conv2D(128, 3, activation="relu", strides=2, padding="same", kernel_initializer=initializer)(cnn_inputs)
+x = layers.Conv2D(256, 3, activation="relu", strides=2, padding="same", kernel_initializer=initializer)(x)
+# x = layers.SpatialDropout2D(DROPOUT)(x)
+x = layers.Conv2D(512, 3, activation="relu", strides=1, padding="same", kernel_initializer=initializer)(x)
+x = layers.Flatten()(x)
+x = layers.Dense(LATENT_DIM)(x)
+initializer = initializers.GlorotNormal()
+outputs = layers.Dense(10, kernel_initializer = initializer, activation="softmax")(x)
+cnn = keras.Model(inputs=cnn_inputs, outputs=outputs)
+if USE_PRETRAIN:
+    cnn.layers[1].trainable = False
+    cnn.layers[2].trainable = False
+    cnn.layers[3].trainable = False
+cnn.summary()
+
+adam_optimizer = optimizers.Adam()
+
+cnn.compile(optimizer=adam_optimizer,
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+# Initialize weights (pretraining)
+if os.path.isfile(VAE_LOCATION):
+    # vae = keras.Model()
+    # vae.compile()
+    # vae.call()
+    # vae.build(input_shape = DATA_SHAPE)
+    vae.fit(x_train[0:10], epochs=1, batch_size=BATCH_SIZE, verbose=3)
+    vae.load_weights(VAE_LOCATION)
+    # vae = keras.models.load_model(VAE_LOCATION)
+
+if USE_PRETRAIN:
+    cnn.layers[1].set_weights(vae.encoder.layers[1].get_weights())
+    cnn.layers[2].set_weights(vae.encoder.layers[2].get_weights())
+    cnn.layers[3].set_weights(vae.encoder.layers[3].get_weights())
+    cnn.layers[4].set_weights(vae.encoder.layers[4].get_weights())
+
+cnn.fit(x_train, y_train, epochs=10, batch_size=BATCH_SIZE, validation_split=0.2, verbose=1)
